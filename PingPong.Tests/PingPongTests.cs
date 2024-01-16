94af2a179e2859352bc5f1e5a08bc4b567613917
Jonathan Song <jonats10@uci.edu>
2024-01-16 14:52:27 -0800

    finish logic

---
+++ PingPong.Tests/PingPongTests.cs
@@ -0,0 +1,14 @@
+// TextReplacer
+// will return "ping" if divisible by 3
+// will return "pong" if divisible by 5
+// will return "ping-pong" if divisible by both 3 and 5
+
+
+// Counter
+// It will return 0 if inputted number is 0
+// It will use a for loop to count up to the specified number, replacing divisible numbers with strings from TextReplacer
+
+
+
+
+
---
+++ PingPong/Program.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading;
+
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("                         +++++++++++++++++");
+    Console.WriteLine("                         Enter a Number:");
+    Console.WriteLine("                         +++++++++++++++++");
+
+    if (int.TryParse(Console.ReadLine(), out int userInput))
+    {
+      ConsoleColor[] colors = {
+        ConsoleColor.DarkRed,
+        ConsoleColor.Red,
+        ConsoleColor.DarkMagenta,
+        ConsoleColor.Magenta,
+        ConsoleColor.DarkYellow,
+        ConsoleColor.Yellow,
+        ConsoleColor.DarkGreen,
+        ConsoleColor.Green,
+        ConsoleColor.DarkBlue,
+        ConsoleColor.Blue,
+        ConsoleColor.Cyan,
+          };
+
+      for (int i = 1; i <= userInput; i++)
+      {
+        Thread.Sleep(100);
+        ApplyColor(colors[i % colors.Length]);
+
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+          Console.WriteLine("Ping Pong");
+        }
+        else if (i % 5 == 0)
+        {
+          Console.WriteLine("Pong");
+        }
+        else if (i % 3 == 0)
+        {
+          Console.WriteLine("Ping");
+        }
+        else
+        {
+          Console.WriteLine(i);
+        }
+      }
+    }
+    else
+    {
+      Console.WriteLine("Enter a Number: ");
+    }
+  }
+  static void ApplyColor(ConsoleColor colors)
+  {
+    Console.ForegroundColor = colors;
+  }
+
+}
